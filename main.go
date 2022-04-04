ccd34ab36ec4e895d0549769eae1d9a2446ba636
Tombe Mogga <tombe.mogga@digicert.com>
2022-04-04 11:44:07 -0600

    Started Server goroutine

--- main.go
+++ main.go
@@ -1,1 +1,44 @@
 package main
+
+import "encoding/json"
+
+type ClientManager struct {
+	clients    map[*Client]bool
+	broadcast  chan []byte
+	register   chan *Client
+	unregister chan *Client
+}
+
+type Client struct {
+	id     string
+	socket *websocket.Conn
+	send   chan []byte
+}
+
+type Message struct {
+	Sender    string `json:"sender,omitempty"`
+	Recipient string `json:"recipient,omitempty"`
+	Content   string `json:"content,omitempty"`
+}
+
+var manager = ClientManager{
+	broadcast:  make(chan []byte),
+	register:   make(chan *Client),
+	unregister: make(chan *Client),
+	clients:    make(map[*Client]bool),
+}
+
+func (manager *ClientManager) start() {
+	for {
+		select {
+		case conn := <-manager.register:
+			manager.clients[conn] = true
+			jsonMessage, _ := json.Marshal(&Message{Content: "/A new socket has connected."})
+			manager.send(jsonMessage, conn)
+		case conn := <-manager.unregister:
+			if _, ok := manager.clients[conn]; ok {
+				close(conn.send)
+			}
+		}
+	}
+}
